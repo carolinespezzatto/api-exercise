9d85709d55fca612c566a249211e6a6e6435e67f
Caroline Spezzatto <caroline.spezzatto@gmail.com>
2021-07-05 11:07:25 -0300

    api learning

--- 01/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require ('axios')
-
-axios.get('https://www.google.com/')
-    .then (function(res) {
-        console.log(res)
-    })  
-    .catch (function(error) {
-        console.log(error)
-    })  
--- 05/app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-fs.writeFile('criando-arquivo.txt','Meu arquivo txt', function (erro) {
-    if(erro) {
-        throw erro
-    }
-    console.log('Arquivo salvo!')
-})
